a213d77347af8af2d80c71318122d861d75cb30e
bhavyanairmv <73218557+bhavyanairmv@users.noreply.github.com>
2020-12-27 10:25:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+var bullet , wall;
+var speed , weight , thickness;
+
+function setup() 
+{
+  createCanvas(1600 ,400);
+
+  speed = random(223 , 321);
+  weight = random(30 , 52);
+  thickness = random(22 ,83 );
+
+  bullet = createSprite(50 , 200 , 50 , 5);
+  bullet.shapeColor =color (225);
+  bullet.velocityX = speed;
+
+  wall = createSprite(1200 , 200 , thickness , height/2);
+  wall.shapeColor = color (80 , 80 , 80);
+
+  
+}
+
+function draw() {
+  background(0);  
+
+  if(hascollided(bullet  ,  wall))
+  {
+      bullet.velocityX = 0;
+      var damage = 0.5 * weight * speed * speed / (thickness * thickness * thickness);
+
+      if(damage > 10)
+      {
+           wall.shapeColor = color(225 , 0 , 0);
+      }
+
+      if(damage < 10)
+      {
+           wall.shapeColor = color(0 , 255 , 0);
+      }
+
+     
+  }
+  drawSprites();
+}
+
+function hascollided(lbullet , lwall)
+{
+     bulletRightEdge = lbullet.x + lwall.width;
+     wallLeftEdge = lwall.x;
+      
+     if(bulletRightEdge >= wallLeftEdge)
+     {
+          return true
+     } 
+          return false;
+}
